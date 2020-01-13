65316b3bbb806d4345e34fdd172652288ad9c974
salvadora <salvadora@zam-id-saac>
2020-01-13 16:08:39 -0600

    Seccion 7 video 72

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$imHgrb/i2dAy/0BEaxlk1eFwdWPeuNJcFQq0lEGjhDLwBTIP0wL7W',true, 'Andres', 'Guzman','profesor@bolsadeideas.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$Wxtk3mSKaZw6HmUfc9Dj/O2qdEeJFj1YlvX02w4p42zCAiIt2fcMq',true, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$wtnRJHx.1lPf7Chlglrj0.N6Z215S0JLENroWe/kpjiU0WJvUJoGK',true, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$oDi2Ae5uqOss0I23s9r1WeH1J4ET9Ddettc8dhP5k0yVKX0Z72niW',true, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
